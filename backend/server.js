e4b3abe859eb7ebd313a34a978ea075f9b3550d0
Philipp H <philipp.hochmann@gmx.at>
2019-06-05 15:18:18 +0200

    removed http server

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,7 @@
-const http = require('http');
 const app = require('./app/app');
 
 const PORT = 3000;
 
-const server = http.createServer(app);
-server.on("listening", ()=> {
+app.listen(PORT, () => {
     console.log("Server started listening on http://localhost:3000");
-})
-server.listen(PORT);
+});
