7596c9456b51b9ac3a5781f07f9e49554c1de92b
Philipp H <philipp.hochmann@gmx.at>
2019-06-05 16:12:50 +0200

    added get groups

--- node-client/utils/groups.js
+++ node-client/utils/groups.js
@@ -1,2 +1,21 @@
 const request = require('request');
 
+const getGroups = (callback) => {
+    const url = "http://localhost:3000/api/groups/";
+    request({url, json: true}, (error, {body}) => {
+        if(error) {
+            callback('Unable to connect to service.');
+            return;
+        }
+        let groups = []
+        body.forEach(group => {
+            groups.push({
+                id: group._id,
+                name: group.name
+            });
+        });
+        callback(undefined, groups);
+    });
+};
+
+module.exports = {getGroups};
